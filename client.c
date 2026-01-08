67b14116ab29ec56fbe719410c04c885773e4c8b
Zeotical <aceyems10@gmail.com>
2026-01-08 15:30:59 +0800

    Client (unneeded code removed)

--- client.c
+++ client.c
@@ -11,12 +11,6 @@
 #include <sys/socket.h>
 #define PORT 8080
 
-//const long MAX_MSG_SIZE = 256; // Must match sender's size
-// Define the data structure for the message (must be identical)
-struct Message {
-int counter;
-char buffer[252];
-};
 int main(int argc, char* argv[]) {
 
 // Socket Code provided by Mr Sharaf
@@ -25,7 +19,7 @@ int sock = 0;
 struct sockaddr_in serv_addr;
 //ClientState state;
 //char buffer [256] = {0};
-    int status, valread, client;
+int status, valread, client;
     
 char buffer[1024] = { 0 };
 
@@ -44,48 +38,11 @@ printf("Connection failed\n");
 return -1;
 }
   printf("Connection to server established\n");  
-  
-
-// if (argc != 2) {
-// fprintf(stderr ,"Usage: %s <MQ_NAME>" , argv[0]);
-// return 1;
-// }
 
 valread = read(sock, buffer,1024 - 1); 
 printf("%s\n", buffer);
 
-// const char* MQ_NAME = argv[1];
-// mqd_t mqd;
-// struct Message msg;
-// 1. Open the existing message queue (read-only)
-// Note: We don't need the attributes struct here, as we are not creating it.
-// mqd = mq_open(MQ_NAME, O_RDONLY);
-// if (mqd == (mqd_t)-1) {
-// perror("\n[RECEIVER] mq_open failed");
-// return 1;
-// }
-// printf ("\n[RECEIVER] Child Process (PID: %d) connected to message queue." , getpid()) ;
-
-// // 2. Continuously receive messages until termination signal (-1)
-// while (true) {
-// // Receive the message. It blocks until a message is available.
-// ssize_t bytes_read = mq_receive(mqd, (char*)&msg, MAX_MSG_SIZE, NULL);
-// if (bytes_read == -1) {
-// perror("\n[RECEIVER] mq_receive failed");
-// break;
-// }
-
-// if (msg.counter == -1) {
-// printf ("\n[RECEIVER] Received termination signal. Exiting." ) ;
-// break;
-// }
-// printf( "\n[RECEIVER] Received Counter: %d  | Message: %s", msg.counter, msg.buffer);
-// }
-
 // Cleanup: Close the message queue descriptor
 close(client);
-//mq_close(mqd);
-// close(client);
-
 return 0;
 }
